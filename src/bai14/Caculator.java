f20763fe437c804cafc9481639f3fc2aa1afedbc
ThinhRikei <tranvanthinh786@gmail.com>
2024-01-17 11:23:27 +0700

    them bai 14

---
+++ src/bai14/Caculator.java
@@ -0,0 +1,19 @@
+package bai14;
+
+public class Caculator {
+    public static void main(String[] args) {
+        String str1=" aka jnf   aaa a b c ";
+        System.out.println("số lượng từ chuỗi"+str1.length());
+        System.out.println(countWord(str1,3));
+    }
+    public static int countWord(String text,int a){
+        String[] newString=text.trim().split(" ");
+        int count=0;
+        for (int i = 0; i < newString.length; i++) {
+            if (newString[i].length()>=a){
+               count+=1;
+            }
+        }
+        return count;
+    }
+}
---
+++ test/ValidateTest.java
@@ -0,0 +1,11 @@
+import bai13.Main;
+import org.junit.jupiter.api.Test;
+
+public class ValidateTest {
+    Main main=new Main();
+
+    @Test
+    public void test1(){
+
+    }
+}
